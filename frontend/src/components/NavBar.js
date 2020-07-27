55c3e4f2f472b4fe6e6391bfd4dbd0165612bbbd
azanbinzahid <azan.zahid@arbisoft.com>
2020-07-27 10:58:23 +0500

    add styled components

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
+import styled from 'styled-components'
 
-const NavItem = (to, menu) => {
+
+const Item = styled.h4``;
+const Title = styled.h2`
+    font-weight: bold;
+    color: blue;
+`;
+
+
+const NavItem = (toPath, menuName) => {
     return (
-        <LinkContainer to={to}>
+        <LinkContainer to={toPath}>
             <Nav.Link>
-                <h4>
-                    {menu}
-                </h4>
+                <Item>
+                    {menuName}
+                </Item>
             </Nav.Link>
         </LinkContainer>
     )
@@ -18,7 +27,7 @@ const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg">
         <LinkContainer to="/">
-        <Navbar.Brand> <h2> Job Portal </h2></Navbar.Brand>
+        <Navbar.Brand> <Title> Job Portal </Title></Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
