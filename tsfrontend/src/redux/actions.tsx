edec6ebdbdb9c884e61706221bfabf1704971516
azanbinzahid <azan.zahid@arbisoft.com>
2020-08-07 17:43:27 +0500

    action template

---
+++ tsfrontend/src/redux/actions.tsx
@@ -0,0 +1,11 @@
+import uuid from 'uuid'
+import axios from 'axios'
+import {Dispatch} from 'redux'
+import {
+UserActionTypes,    
+JobActionTypes,    
+AlertActionTypes,
+LogOutUserAction,
+SetAlertAction,    
+RemoveAlertAction
+} from 'redux/types'
