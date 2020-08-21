0c01392d7426ad8f3c289b660adf6f95e76a1bdd
azanbinzahid <azan.zahid@arbisoft.com>
2020-08-21 19:51:04 +0500

    toggle headaer color

--- andoirdapp/src/App.js
+++ andoirdapp/src/App.js
@@ -9,12 +9,17 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './store';
+import getTheme from '../native-base-theme/components';
+import material from '../native-base-theme/variables/material';
+import {StyleProvider} from 'native-base';
 import Router from './components/Router';
 
 const App = () => (
-  <Provider store={store}>
-    <Router />
-  </Provider>
+  <StyleProvider style={getTheme(material)}>
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </StyleProvider>
 );
 
 export default App;
