12d1d9140adf86ee3d38b0de5ad47773041c32b3
azanbinzahid <azan.zahid@arbisoft.com>
2020-08-28 17:27:32 +0500

    user profile model tests

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+import datetime
 
-# Create your tests here.
+
+class ProfileModelTests(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="test")
+        user.profile.bio = "test bio"
+        user.profile.location = "test location"
+        user.profile.education = "test education"
+        user.profile.experiance = "test experiance"
+        user.profile.birth_date = datetime.datetime.now()
+        user.profile.save()
+
+    def test_profile(self):
+        user = User.objects.get(username="test")
+        self.assertEqual(user.profile.bio, "test bio")
+        self.assertEqual(user.profile.location, "test location")
+        self.assertEqual(user.profile.education, "test education")
+        self.assertEqual(user.profile.experiance, "test experiance")
